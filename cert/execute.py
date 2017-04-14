b7a31778e6442bd4638d4adaeb2f9057d0584313
egoroff <egoroff@gmail.com>
2017-04-14 22:01:26 +0300

    code style fixes

--- cert/execute.py
+++ cert/execute.py
@@ -1,9 +1,12 @@
-__author__ = 'egr'
-
 import subprocess
+
+__author__ = 'egr'
 
 
 class Execute:
+    def __init__(self):
+        pass
+
     @staticmethod
     def run_proc(params):
         proc = subprocess.Popen(params)
